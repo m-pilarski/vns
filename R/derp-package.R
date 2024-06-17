bd2addc4e3ad9bdb1552575a4b037540b5385936
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2024-06-17 14:40:53 +0200

    update

---
+++ R/derp-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom methods is
+#' @importFrom utils menu
+## usethis namespace: end
+NULL
--- R/sentiment_transformer.R
+++ R/sentiment_transformer.R
@@ -4,21 +4,25 @@ library(reticulate)
 `%<-%` <- zeallot::`%<-%`
 `%||%` <- rlang::`%||%`
 
-amazon_review_tbl <- fst::read_fst(here::here("data/amazon_review_tbl.fst"))
+# amazon_review_tbl <- fst::read_fst(here::here("data/amazon_review_tbl.fst"))
 
 
 load_germansentiment_model <- function(){
-  .lib_germansentiment <- import("germansentiment")
-  .germansentiment_model <- lib_germansentiment$SentimentModel()
+  .lib_germansentiment <- reticulate::import("germansentiment")
+  .germansentiment_model <- .lib_germansentiment$SentimentModel()
   return(.germansentiment_model)
 }
 
 
-calc_sentiment <- function(.doc_str, .germansentiment_model){
+# lib_builtin <- reticulate::import_builtins()
+
+calc_sentiment <- function(
+  .doc_str, .germansentiment_model=load_germansentiment_model()
+){
   checkmate::assert_character(.doc_str, len=1, any.missing=FALSE)
   .doc_str |>
     as.list() |>
-    sentiment_model$predict_sentiment(output_probabilities=TRUE) |>
+    .germansentiment_model$predict_sentiment(output_probabilities=TRUE) |>
     purrr::transpose() |>
     purrr::map(\(..doc_pred){
       ..doc_pred |>
@@ -34,12 +38,12 @@ calc_sentiment <- function(.doc_str, .germansentiment_model){
     dplyr::bind_rows()
 }
 
-checkmate::assert_character()
+# checkmate::assert_character()
 
-calc_sentiment_memo <- memoise::memoize(calc_sentiment)
+# calc_sentiment_memo <- memoise::memoize(calc_sentiment)
 
-calc_doc_sentiment_tbl <- function(.doc_vec, ..., .cache_dir=NULL){
-  purrr::map_dfr(.doc_vec, \(..doc){
-    c(doc_str=..doc, calc_sentiment_memo(..doc))
-  })
-}
+# calc_doc_sentiment_tbl <- function(.doc_vec, ..., .cache_dir=NULL){
+#   purrr::map_dfr(.doc_vec, \(..doc){
+#     c(doc_str=..doc, calc_sentiment_memo(..doc))
+#   })
+# }
