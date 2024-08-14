cafb34a3aa216422b51c8dfc8747dacaecd058bc
m-pilarski <pilarski@cryptolab.net>
2024-08-14 21:10:42 +0200

    update

--- R/setup_python.R
+++ R/setup_python.R
@@ -5,7 +5,7 @@ python_dependencies <- c("germansentiment", "spacy")
 #' FUNCTION_DESCRIPTION
 #'
 #' @param .conda_envname DESCRIPTION.
-#' @param .install_conda DESCRIPTION.
+#' @param .install_miniconda DESCRIPTION.
 #' @param .create_condaenv DESCRIPTION.
 #'
 #' @return RETURN_DESCRIPTION
@@ -13,29 +13,28 @@ python_dependencies <- c("germansentiment", "spacy")
 #' # ADD_EXAMPLES_HERE
 #' @export
 setup_vns_condaenv <- function(
-  .conda_envname="vns_condaenv", .install_conda=NULL, .create_condaenv=NULL
+  .conda_envname="vns_condaenv", .install_miniconda=NULL, .create_condaenv=NULL
 ){
   checkmate::assert(
     checkmate::check_character(
       .conda_envname, len=1, null.ok=FALSE, any.missing=FALSE
     ),
     checkmate::check_logical(
-      .install_conda, len=1, null.ok=TRUE, any.missing=FALSE
+      .install_miniconda, len=1, null.ok=TRUE, any.missing=FALSE
     ),
     checkmate::check_logical(
       .create_condaenv, len=1, null.ok=TRUE, any.missing=FALSE
     )
   )
   if(is(try(reticulate::conda_version()), "try-error")){
-    if(is.null(.install_conda)){
+    if(is.null(.install_miniconda)){
       if(interactive()){
-        .install_miniconda <- identical(
+        .install_miniconda <-
           menu(
             title=crayon::italic("No conda installation found"),
             choices=c("run reticulate::install_miniconda()", "abort")
-          ),
-          1L
-        )
+          ) |>
+          identical(1L)
       }else{
         .install_miniconda <- FALSE
       }
@@ -45,20 +44,19 @@ setup_vns_condaenv <- function(
     }else{
       rlang::abort(c(
         "Missing a conda installation.",
-        i="Please rerun with .install_conda=TRUE or install conda manually"
+        i="Please rerun with .install_miniconda=TRUE or install conda manually"
       ))
     }
   }
   if(!.conda_envname %in% reticulate::conda_list()$name){
     if(is.null(.create_condaenv)){
       if(interactive()){
-        .create_condaenv <- identical(
+        .create_condaenv <-
           menu(
             title=crayon::italic("The conda environment does not exist"),
             choices=c("run reticulate::conda_create(.conda_envname)", "abort")
-          ),
-          1L
-        )
+          ) |>
+          identical(1L)
       }else{
         .create_condaenv <- FALSE
       }
