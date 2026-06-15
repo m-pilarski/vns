318babda594476763f22baeaf8751d280ba806a3
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2026-06-15 19:19:22 +0200

    update

--- R/extract_content_html.R
+++ R/extract_content_html.R
@@ -13,6 +13,50 @@
 extract_content_html <- function(.html) {
   checkmate::assert_character(.html, min.len = 1, any.missing = FALSE, null.ok = FALSE)
 
+  .with_restored_sink <- function(.expr) {
+    .sink_count <- sink.number()
+    on.exit({
+      while (sink.number() > .sink_count) {
+        sink(NULL)
+      }
+    }, add = TRUE)
+    force(.expr)
+  }
+
+  .readability_input_html <- function(..html) {
+    ..doc <- xml2::read_html(..html)
+    xml2::xml_remove(xml2::xml_find_all(
+      ..doc,
+      paste(
+        ".//script[not(@type='application/ld+json')]",
+        ".//style",
+        ".//noscript",
+        ".//nav",
+        ".//header",
+        ".//footer",
+        ".//*[contains(concat(' ', normalize-space(@class), ' '), ' teaser ')]",
+        ".//*[contains(concat(' ', normalize-space(@class), ' '), ' share ')]",
+        sep = "|"
+      )
+    ))
+
+    .candidates <- xml2::xml_find_all(..doc, ".//article|.//main")
+    .node <- if (length(.candidates) > 0) {
+      .candidates[[which.max(nchar(xml2::xml_text(.candidates)))]]
+    } else {
+      xml2::xml_find_first(..doc, ".//body")
+    }
+
+    .title <- xml2::xml_text(xml2::xml_find_first(..doc, ".//title"))
+    paste0(
+      "<!doctype html><html><head><title>",
+      htmltools::htmlEscape(.title),
+      "</title></head><body>",
+      as.character(.node),
+      "</body></html>"
+    )
+  }
+
   .ctx <- V8::v8()
   .ctx$eval(
     readr::read_file(
@@ -29,7 +73,16 @@ extract_content_html <- function(.html) {
       "function parseWithReadability(html){",
       "  const parser = new JSDOMParser();",
       "  const doc = parser.parse(html, 'about:blank');",
-      "  const article = new Readability(doc).parse();",
+      "  if (doc && !doc.documentElement) {",
+      "    for (const child of doc.childNodes || []) {",
+      "      if (child.localName === 'html') {",
+      "        doc.documentElement = child;",
+      "        break;",
+      "      }",
+      "    }",
+      "    if (!doc.documentElement && doc.firstChild) doc.documentElement = doc.firstChild;",
+      "  }",
+      "  const article = new Readability(doc, {charThreshold: 250, keepClasses: false}).parse();",
       "  return (article && article.content) ? article.content : '';",
       "}",
       sep="\n"
@@ -39,9 +92,11 @@ extract_content_html <- function(.html) {
   .content <- purrr::map_chr(.html, function(..html) {
     tryCatch(
       expr = {
-        sink(file = nullfile())
-        ..content <- .ctx$call("parseWithReadability", ..html)
-        sink(file = NULL)
+        ..readability_html <- .readability_input_html(..html)
+        ..content <- .with_restored_sink({
+          sink(file = nullfile())
+          .ctx$call("parseWithReadability", ..readability_html)
+        })
       },
       error = \(..e) {
         rlang::abort(c(
